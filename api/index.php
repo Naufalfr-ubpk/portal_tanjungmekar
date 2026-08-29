e96ebb49d14b350ae9ea50aaa41a45280da4a3c4
Naufal Fauzi Rahman <myth1c4lg0d5l4yer@gmail.com>
2026-08-29 08:53:37 +0700

    fix: clear windows cache and force tmp directory

--- api/index.php
+++ api/index.php
@@ -22,10 +22,15 @@ foreach ($directories as $dir) {
     }
 }
 
-// 2. Arahkan direktori view agar tidak nyasar ke folder yang dikunci
+// 2. Paksa Laravel simpan cache bootstrap ke /tmp (Bypass cache Windows)
+$_ENV['APP_SERVICES_CACHE'] = "{$storagePath}/bootstrap/cache/services.php";
+$_ENV['APP_PACKAGES_CACHE'] = "{$storagePath}/bootstrap/cache/packages.php";
+$_ENV['APP_CONFIG_CACHE']   = "{$storagePath}/bootstrap/cache/config.php";
+$_ENV['APP_ROUTES_CACHE']   = "{$storagePath}/bootstrap/cache/routes.php";
+$_ENV['APP_EVENTS_CACHE']   = "{$storagePath}/bootstrap/cache/events.php";
 putenv("VIEW_COMPILED_PATH={$storagePath}/framework/views");
 
-// 3. Panggil aplikasi Laravel
+// 3. Panggil aplikasi Laravel 12
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 // 4. Timpa path storage bawaan Laravel ke /tmp
